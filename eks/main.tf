892ccc65d7638cfbad667b0ecca4d114cc8dd89f
m0c1 <mike@onpremcloud.us>
2023-12-29 18:26:38 -0500

    cluster name update

--- eks/main.tf
+++ eks/main.tf
@@ -10,7 +10,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "acloudguy-eks-${random_string.suffix.result}"
+  cluster_name = "2pac-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
