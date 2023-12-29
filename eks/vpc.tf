34f7eec7b13b1480c10c393dd613bdd4a247fa8c
m0c1 <mike@onpremcloud.us>
2023-12-29 18:27:26 -0500

    Update vpc.tf

--- eks/vpc.tf
+++ eks/vpc.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.14.2"
 
-  name = "acloudguy-vpc"
+  name = "2pac-vpc"
 
   cidr = "10.0.0.0/16"
   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
